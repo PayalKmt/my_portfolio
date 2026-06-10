2329ed2bb3dfc5f69a29cf5fbfec86835466dfc4
PayalKmt <kumawatpayal2005513@gmail.com>
2026-06-10 13:35:29 +0530

    fix: use html renderer and relax sdk constraint for vercel build

--- build_vercel.sh
+++ build_vercel.sh
@@ -4,5 +4,6 @@ set -e
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable
 export PATH="$PATH:$(pwd)/flutter/bin"
 flutter config --enable-web
+flutter --version
 flutter pub get
-flutter build web --release --no-tree-shake-icons
+flutter build web --release --no-tree-shake-icons --web-renderer html
